275cf22783d433f3b7827b1aad5fff924be53d61
Halyson Reis <halysondasilvadosreis@gmail.com>
2024-05-27 23:00:02 -0300

    Update quero.py

--- basico de python/testando/flet/quero.py
+++ basico de python/testando/flet/quero.py
@@ -11,12 +11,18 @@ def main(page: ft.Page):
         ])
     )
 
-    testo = ft.Text()
-    page.add(testo) # it's a shortcut for page.controls.append(t) and then page.update()
-
+    texto = ft.Text()
+    page.add(texto) # it's a shortcut for page.controls.append(t) and then page.update()
+    
     for i in range(10):
-        time.sleep(1)
-        testo.value = f"Step {i}"
+        page.controls.append(ft.Text(f"Line {i}"))
+        if i > 4:
+            page.controls.pop(0)
+        texto.value = f"step {i}"
         page.update()
+        time.sleep(0.3)
 
+    def button_clicked(e):
+        page.add(ft.Text("Clicked!"))
+    page.add(ft.ElevatedButton(text="registar", on_click=button_clicked))
 ft.app(target=main)
